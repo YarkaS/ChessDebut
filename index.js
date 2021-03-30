9e88644a58ed886a505c042bbd16a8cc03da6581
YarkaS <y2096r@gmail.com>
2021-03-30 14:47:15 -0400

    fix for Heroku

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const path = require('path');
 const app = require("express")();
 const PORT = process.env.PORT || 3001;
 
-app.get("/Back-End",  (req, res) => {res.json({message:"This is ChessDebut 1.0"});});
+// for the Node to use the files of React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/Back-End",  (req, res) => {res.json({message:"This is ChessDebut 1.0 - Server!"});});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 app.listen(PORT, () => {console.log('App up at port ${PORT}');});
