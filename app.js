d2d5d5b349431fac018b2903272bab96d23e127e
YarkaS <65980867+YarkaS@users.noreply.github.com>
2021-03-30 13:01:46 -0400

    Revert "Jesse"

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
 
-const express = require('express');
-const app = express();
-const cors = require('cors');
-
-const Router = require('./Back-End/Router/API.js');
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(cors());
-app.use(Router);
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on Port ${PORT}`);
-});
-
+app.get("",  (req, res) => {res.send("This is ChessDebut 1.0");});
+app.listen(PORT, () => {console.log('App up at port ${PORT}');});
