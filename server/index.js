63df4f9efed71af0734d808933312b1ce50bc0c5
YarkaS <y2096r@gmail.com>
2021-03-30 16:04:52 -0400

    updated location

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// for the Node to use the files of React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/Back-End",  (req, res) => {res.json({message:"This is ChessDebut 1.0 - Server!"});});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+app.listen(PORT, () => {console.log('App up at port ${PORT}');});
