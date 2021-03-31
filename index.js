b7f2cdeb93ea53ddb2b7bf3e11d42e51415f2751
YarkaS <y2096r@gmail.com>
2021-03-31 14:29:17 -0400

    Home page

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // for the Node to use the files of React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
+//express-static middleware
+app.use(express.static("public"))
 
 app.get("/Back-End",  (req, res) => {res.json({message:"This is ChessDebut 1.0 - Server!"});});
+app.get("/", function (req,res) {
+  res.send("<h1> Chess Debuts are here! </h1>")
+})
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../ChessDebuts/client/build', 'index.html'));
 });
