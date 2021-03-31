5475b7c30fc5b13d26784e51930080e949e08df2
YarkaS <y2096r@gmail.com>
2021-03-30 22:19:55 -0400

    Fixed the connection between the React and Node

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/Back-End",  (req, res) => {res.json({message:"This is ChessDebut 1.0 - Server!"});});
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../ChessDebuts/client/build', 'index.html'));
 });
 app.listen(PORT, () => {console.log('App up at port ${PORT}');});
