cab64326e0823fa48662e63f09006f93efe35ae8
Kanackle <kanak.somani02@myhunter.cuny.edu>
2021-04-03 16:20:28 -0400

    Some styling changes

--- client/src/Play.js
+++ client/src/Play.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
- 
+import './index.css';
+
 class Play extends Component {
   render() {
     return (
       <div>
         <h2>Take a quiz!</h2>
         <p>Levels</p>
-        <ol>
-          <li>Easy</li>
-          <li>Medium</li>
-          <li>Hard</li>
-
+        <ol className="quiz">
+          <ul className="quizButtons"><button>Easy</button></ul>
+          <ul className="quizButtons"><button>Medium</button></ul>
+          <ul className="quizButtons"><button>Hard</button></ul>
+          <playGame />
         </ol>
       </div>
     );
