1ae523b1ab2fbe60ec872be2965b0d046452c196
YarkaS <y2096r@gmail.com>
2021-04-04 00:14:33 -0400

    Basic quiz

---
+++ client/src/fake_api/QuizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "Given the Ruy Lopez. What is the Correct move order",
+      answers: [
+          {
+              type: "correct",
+              content: "e2e4 e7e5 g1f3 b8c6 f1b5"
+          },
+          {
+              type: "incorrect",
+              content: "g1f3 g8f6 g2g3 d7d5"
+          },
+          {
+              type: "wrong",
+              content: "b2b3"
+          }
+      ]
+  },
+  {
+      question: "You want to play the Benoni Defense. What are the first moves?",
+      answers: [
+          {
+              type: "correct",
+              content: "d2d4"
+          },
+          {
+              type: "incorrect",
+              content: "d2d4"
+          },
+          {
+              type: "wrong",
+              content: "e2e4"
+          }
+      ]
+  },
+  {
+      question: "What is this opening? e2e4 e7e5 g1f3 b8c6 f1b5",
+      answers: [
+          {
+              type: "correct",
+              content: "Ruy Lopez"
+          },
+          {
+              type: "incorrect",
+              content: "Caro-Kann Defense"
+          },
+          {
+              type: "wrong",
+              content: "Sicilian defense "
+          }
+      ]
+  },
+  {
+      question: "Given the Catalan Opening. What is the Correct move order",
+      answers: [
+          {
+              type: "correct",
+              content: "d2d4 g8f6 c2c4 e7e6 g2g3"
+          },
+          {
+              type: "incorrect",
+              content: "e2e4 e7e5 g1f3 b8c6 "
+          },
+          {
+              type: "wrong",
+              content: "g1f3 g8f6 g2g3 d7d5"
+          }
+      ]
+  },
+  {
+      question: "You want to play the Amar Opening. What are the first moves?",
+      answers: [
+          {
+              type: "correct",
+              content: "g1h3"
+          },
+          {
+              type: "incorrect",
+              content: "g1h3"
+          },
+          {
+              type: "wrong",
+              content: "b2b3"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
