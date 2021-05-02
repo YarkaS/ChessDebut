71f9ffdf250dce192385d3976738007b14863868
YarkaS <y2096r@gmail.com>
2021-05-02 13:13:32 -0400

    fixed ui

--- client/src/Dictionary.js
+++ client/src/Dictionary.js
@@ -7,14 +7,7 @@ class Dictionary extends Component {
   render() {
     return (
       <div>
-        <h2>The dictionary</h2>
-        <p>
-          List of chess{" "}
-          <a href="https://en.wikipedia.org/wiki/List_of_chess_openings">
-            openings
-          </a>
-          .
-        </p>
+       
         <Container fluid>
           <Row className = "Row">
             <Col className = "cols">
