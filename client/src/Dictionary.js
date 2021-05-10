22afa7ece7c55d60291955e2e9ef4bb41dccd7f1
Kanackle <kanak.somani02@myhunter.cuny.edu>
2021-05-10 12:03:06 -0400

    Adding a form component(will add links later)

--- client/src/Dictionary.js
+++ client/src/Dictionary.js
@@ -12,17 +12,61 @@ import ImageDemo from "./ImageDisplay";
 
 
 function App() {
+
+  const [ selected, setSelected ] = useState(null);
+
+let openings = [{
+  id: 1,
+  value: "Alekhine's Defense",
+  picture: "./Imgs/Alekhine.jpeg",
+},
+{
+  id: 2,
+  value: "Amar Opening",
+  picture: "./Imgs/Amar.jpeg",
+},
+{
+  id: 3,
+  value: "Benoni Opening",
+  picture: <img src={require("./Imgs/Benoni.jpeg").default} />,
+},
+{
+  id: 4,
+  value: "Bird's Opening" ,
+  picture: <img src="" />,
+  moveOrder: "1.f4",
+  description: "White's purpose is to gain control of the e5 square." + 
+                "This opening provides a good attack for White at the expense of a weak kingside." +
+                "Black's best response is to play d5." + 
+                "From here, White has multiple ways to maintain control of the e5 square including moving Nf3 or eventually the bishop to b2.",
+}]
+
+
   return (
     <>
       <h1>Welcome to the Dictionary Page</h1>
       <h2>You can learn more about any opening in our database.</h2>
       <h2>You can view the board state, see the move order, and even read a brief description.</h2>
       <h3>To get started, press on the button</h3>
-      <DrawerForm />
+
+      <select onChange={(e)=> {
+        setSelected(openings[e.target.value])
+      }}>
+        {openings.map((o) => {
+          return(
+            <option value={o.value}>{o.value}</option>
+          )
+        })}
+      </select>
+      {/* <DrawerForm /> */}
+      {
+        
+      }
       <br></br>
       <br></br>
       <br></br>
-      <ImageDemo />
+
+      <ImageDemo  /> 
     </>
   );
 }
