7435e3b200cb5a727d60c3aef3bd638832c7b39e
Kanackle <kanak.somani02@myhunter.cuny.edu>
2021-05-13 17:05:37 -0400

    Added seperate object for imported images

--- client/src/ImageDisplay.js
+++ client/src/ImageDisplay.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Image } from 'antd';
+import alk from './Imgs/Alekhine.jpeg'
+import amar from './Imgs/Amar.jpeg'
+// function importAll(r) {
+//   let images = {};
+//   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+//   return images;
+// }
 
-function ImageDemo(url) {
-  return (
-    <Image
-      width={200}
-      src = {require("./Imgs/Alekhine.jpeg").default}
-      alt="Alekhine's Defense"
-    />
-  );
+// const images = importAll(require.context('./Imgs', false, '/.png/'));
+
+// console.log('these are imgg s ', images)
+const openings = {
+  "Alekhine's Defense" :alk,
+  "Amar Opening" : amar
 }
 
-ReactDOM.render(<ImageDemo />, document.getElementById('root'));
+function ImageDemo(props) {
+
+  let selectedValue;
+
+  if(props.selected) {
+   selectedValue = props.openings.filter((opening) => opening.value === props.selected)
+   console.log('this is value at ', selectedValue)
+  } 
+  if(props?.selected && props?.openings.length) {
+    console.log('SelectedValue ',selectedValue[0])
+
+  }
+  return (
+    <>
+
+       {props?.selected ?
+         <div> this is {selectedValue[0].id} and {selectedValue[0].value} <img src={openings[selectedValue[0].value]}/>
+         </div>
+          : null
+
+       }
+       </>
+
+
+)
+}
+
+// ReactDOM.render(<ImageDemo />, document.getElementById('root'));
 
 export default ImageDemo;
