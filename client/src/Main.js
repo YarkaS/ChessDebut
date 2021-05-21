3634446e5eb36d4ac132cdef070fb6a121c8d51c
YarkaS <y2096r@gmail.com>
2021-05-21 11:44:49 -0400

    reorder menu

--- client/src/Main.js
+++ client/src/Main.js
@@ -17,16 +17,16 @@ class Main extends Component {
           
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/play">Short Quiz</NavLink></li>
             <li><NavLink to="/dictionary">Dictionary</NavLink></li>
+            <li><NavLink to="/play">Short Quiz</NavLink></li>
             <li><NavLink to="/trending">Trending</NavLink></li>
             <li><NavLink to="/about">About Us</NavLink></li>
             
           </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
-            <Route path="/play" component={Play}/>
             <Route path="/Dictionary" component={Dictionary}/>
+            <Route path="/play" component={Play}/>            
             <Route path="/Trending" component={Trending}/>    
             <Route path="/about" component={About}/>            
           </div>
