951d9f6114049fff95ff6efbd62d708e7ec04c06
YarkaS <y2096r@gmail.com>
2021-05-21 11:41:21 -0400

    remove ChessBoard button

--- client/src/Main.js
+++ client/src/Main.js
@@ -7,7 +7,6 @@ import {
 import Home from "./Home";
 import Play from "./Play";
 import Dictionary from "./Dictionary"; 
-import Board from "./Board";
 import Trending from "./Trending";
 import About from "./About";
 class Main extends Component {
@@ -20,7 +19,6 @@ class Main extends Component {
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/play">Short Quiz</NavLink></li>
             <li><NavLink to="/dictionary">Dictionary</NavLink></li>
-            <li><NavLink to="/board">Chess Board</NavLink></li>
             <li><NavLink to="/trending">Trending</NavLink></li>
             <li><NavLink to="/about">About Us</NavLink></li>
             
@@ -29,7 +27,6 @@ class Main extends Component {
             <Route exact path="/" component={Home}/>
             <Route path="/play" component={Play}/>
             <Route path="/Dictionary" component={Dictionary}/>
-            <Route path="/Board" component={Board}/> 
             <Route path="/Trending" component={Trending}/>    
             <Route path="/about" component={About}/>            
           </div>
