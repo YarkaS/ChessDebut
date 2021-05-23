1ec56c9dca45b1c7ca13ac7b41551a8ed6c599a0
YarkaS <y2096r@gmail.com>
2021-05-23 12:33:33 -0400

    cleared up trending

--- client/src/Trending.js
+++ client/src/Trending.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import { TwitterTimelineEmbed} from 'react-twitter-embed';
  
 class Trending extends Component {
   render() {
